0700aa4ba11c3781511e7c6d6438806104aeec29
Adam Mesha <adam@mesha.org>
2014-10-24 20:56:11 +0300

    Huge overhaul

--- beelist.py
+++ beelist.py
@@ -2,39 +2,73 @@
 '''Create a basic todo list based on Beeminder goals,
    sorted by how close they are to derailing.'''
 
+import configparser
+import itertools
+import os
+import time
+
 import beeminder
 
 TYPES = {'biker', 'hustler'}
+SECONDS_PER_DAY = 24 * 60 * 60
+IDEAL_DAYS_LEFT = 3
+
+def get_config():
+	config = configparser.ConfigParser(defaults=dict(ideal=IDEAL_DAYS_LEFT),
+	                                   default_section='default',
+	                                   interpolation=None)
+	config.read(os.path.expanduser('~/.beelistrc'))
+	return config
 
 class Beelist:
 
-    def __init__(self, auth_token, format='{title}: {limsum}'):
-        self.auth_token = auth_token
-        self.format = format
-        self._list = None
-        self.bm = beeminder.Beeminder(auth_token)
+	def __init__(self, config, format='{slug}: {limsum}'):
+		self.format = format
+		self._list = None
+		self.now = time.time()
+		self.config = config
+		self.auth_token = config.get('common', 'auth_token')
+		self.bm = beeminder.Beeminder(self.auth_token)
 
-    @property
-    def list(self):
-        if self._list is None:
-            goals = sorted((goal for goal in
-                    self.bm.goals.values()
-                    if goal['goal_type'] in TYPES and not goal['frozen']),
-                key=lambda goal: goal['losedate'])
-            self._list = [self.format.format(**goal) for goal in goals]
-        return self._list
+	def key(self, goal):
+		slug = goal['slug']
+		losedate = goal['losedate']
+		if slug not in self.config:
+			self.config.add_section(slug)
+		ideal = self.config.getint(slug, 'ideal')
+		daysleft = int((losedate - self.now) / SECONDS_PER_DAY)
+		if not daysleft:
+			return daysleft
+		normideal = daysleft / ideal
+		if normideal > 1:
+			return float('inf')
+		default_ideal = self.config.getint('default', 'ideal')
+		return int(normideal * default_ideal)
 
-    def list_str(self, bullet='- [ ] '):
-        seq = [bullet + item for item in self.list]
-        return '\n'.join(seq)
+	@property
+	def list(self):
+		if self._list is None:
+			goals = sorted((goal for goal in
+			                self.bm.goals.values()
+			                if goal['goal_type'] in TYPES and not goal['frozen']),
+			               key=lambda goal: goal['losedate'])
+			groups = itertools.groupby(goals, self.key)
+			self._list = {key: [self.format.format(**goal) for goal in group] for (key, group) in groups}
+		return self._list
+
+	def __str__(self):
+		list = self.list
+		keys = sorted(list.keys())
+		result = []
+		for key in keys:
+			result.append('== Priority {} =='.format(key))
+			result.extend(list[key])
+			result.append('')
+		return '\n'.join(result)
 
 def main():
-    import sys
-    if len(sys.argv) < 2:
-        sys.stderr.write('usage: {} <auth_token>\n'.format(sys.argv[0]))
-        sys.exit(1)
-    auth_token = sys.argv[1]
-    print(Beelist(auth_token).list_str())
+	config = get_config()
+	print(Beelist(config))
 
 if __name__ == '__main__':
-    main()
+	main()
--- beeminder.py
+++ beeminder.py
@@ -3,46 +3,153 @@
 '''Implementation of the Beeminder API: https://www.beeminder.com/api
    Still very incomplete.'''
 
+import json
+import re
+
 import requests
 
-API_URL = 'https://www.beeminder.com/api/v1/'
+class BeeminderMock:
+
+	def __init__(self, mockdata):
+		self.mockdata = mockdata
+
+	@staticmethod
+	def getidp(ath):
+		m = re.search('([a-z0-9]+)\.json$', path)
+		return m.group(1) if m else None
+
+	def execute(self, path, params=None, request_type='get'):
+		print(path, params, request_type)
+		if path.endswith('datapoints.json'):
+			return self.mockdata
+		if request_type == 'delete':
+			dataid = self.getid(path)
+			for (i, pt) in enumerate(self.mockdata):
+			        if pt['id'] == dataid:
+				        del self.mockdata[i]
+				        return pt
+			else:
+				return None
+		raise ValueError((path, params, request_type))
+
+class BeeminderBackend:
+
+	API_URL = 'https://www.beeminder.com/api/v1/'
+
+	def __init__(self, auth_token, dryrun, debug):
+		self.auth_token = auth_token
+		self.dryrun = dryrun
+		self.debug = debug
+
+	def execute(self, path, params=None, request_type='get'):
+		'''Return the url for accessing path, adding the given args
+		and the auth token'''
+		if self.debug:
+			print('{method} {path} {params}'.format(
+				method=request_type.upper(),
+				path=path,
+				params=params or ''
+			))
+		if self.dryrun:
+			print('{} {} {}'.format(request_type.upper(), path, params))
+			key = (path,
+				   tuple(sorted(params.items())) if params is not None else (),
+				   request_type)
+			print(repr(key))
+			return self.dryrun[key]
+		path = path.lstrip('/')
+		method = getattr(requests, request_type)
+		args = {'auth_token': self.auth_token}
+		if params is not None:
+			args.update(params)
+		response = method(self.API_URL + path, params=args)
+		return response.json()
+
 
 class Beeminder:
 
-    def __init__(self, auth_token):
-        self.auth_token = auth_token
-        self._user = None
-        self._goals = {}
+	def __init__(self, auth_token, username=None, dryrun=False, debug=False):
+		self._username = username
+		self._user = None
+		self._goals = {}
+		self._data = {}
+		if not debug:
+			self.backend = BeeminderBackend(auth_token, dryrun, debug)
+		else:
+			import mock
+			self.backend = BeeminderMock(mock.mockdata)
 
-    def get(self, path, args=None):
-        '''Return the url for accessing path, adding the given args
-        and the auth token'''
-        path = path.lstrip('/')
-        response = requests.get(API_URL + path,
-                                params={'auth_token': self.auth_token})
-        return response.json
+	def get(self, path, params=None):
+		return self.backend.execute(path, params, 'get')
 
-    @property
-    def user(self):
-        '''Return the user object'''
-        if self._user is None:
-            self._user = self.get('/users/me.json')
-        return self._user
+	def delete(self, path, params=None):
+		return self.backend.execute(path, params, 'delete')
 
-    def goal(self, slug):
-        '''Return the goal object for the given slug'''
-        if slug not in self._goals:
-            path = '/users/{}/goals/{}.json'.format(
-                self.user['username'], slug)
-            self._goals[slug] = self.get(path)
-        return self._goals[slug]
+	def put(self, path, params=None):
+		return self.backend.execute(path, params, 'put')
 
-    @property
-    def goals(self):
-        path = '/users/{}/goals.json'.format(self.user['username'])
-        goal_list = self.get(path)
-        for goal in goal_list:
-            slug = goal['slug']
-            self._goals[slug] = goal
-        return self._goals
+	def post(self, path, params=None):
+		return self.backend.execute(path, params, 'post')
 
+	@property
+	def user(self):
+		'''Return the user object'''
+		if self._user is None:
+			self._user = self.get('/users/me.json')
+		return self._user
+
+	@property
+	def username(self):
+		'''Return the user's username'''
+		return self._username or self.user['username']
+
+	def goal(self, slug):
+		'''Return the goal object for the given slug'''
+		if slug not in self._goals:
+			path = '/users/{}/goals/{}.json'.format(
+				self.username, slug)
+			self._goals[slug] = self.get(path)
+		return self._goals[slug]
+
+	@property
+	def goals(self):
+		'''Return the list of goals'''
+		path = '/users/{}/goals.json'.format(self.username)
+		goal_list = self.get(path)
+		for goal in goal_list:
+			slug = goal['slug']
+			self._goals[slug] = goal
+		return self._goals
+
+	def data(self, slug):
+		'''Return the datapoints for the given goal'''
+		if slug not in self._data:
+			path = '/users/{}/goals/{}/datapoints.json'.format(
+				self.username, slug)
+			self._data[slug] = self.get(path)
+		return self._data[slug]
+
+	def delete_point(self, slug, data_id):
+		path = '/users/{}/goals/{}/datapoints/{}.json'.format(
+			self.username, slug, data_id)
+		return self.delete(path)
+
+	def update_point(self, slug, data_id,
+					 timestamp=None, value=None, comment=None):
+		path = '/users/{}/goals/{}/datapoints/{}.json'.format(
+			self.username, slug, data_id)
+		params = {}
+		if timestamp is not None:
+			params['timestamp'] = timestamp
+		if value is not None:
+			params['value'] = value
+		if comment is not None:
+			params['comment'] = comment
+		if params:
+			return self.put(path, params=params)
+
+	def create_all(self, slug, points):
+		path = '/users/{}/goals/{}/datapoints/create_all.json'.format(
+			self.username, slug)
+		datapoints = json.dumps(points)
+		self.post(path, params={'datapoints': datapoints})
